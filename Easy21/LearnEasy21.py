ea17324ea8d211dd81aef6ef72101e621fa79b9d
benMen87 <sreterh@gmail.com>
2016-11-11 19:16:49 +0200

    MC controll

--- Easy21/LearnEasy21.py
+++ Easy21/LearnEasy21.py
@@ -1,10 +1,17 @@
 import Easy21
+from collections import namedtuple
+
 
 N0 = 100
 N = dict()
 Q = dict()
+S = namedtuple("easy21_state", ["player_sum", "dealer_sum"])
 
+def creat_state(player_sum, dealer_sum):
+    return S(player_sum=player_sum, dealer_sum=dealer_sum)
 
+def update_N(state, action):
+     
 
 
 def monte_carlos_easy21():
